55e632a913a19d525106b215070c0b4ab08db4d7
Jin Kwon <jin.kwon@xy.company>
2019-05-14 14:43:02 -0700

    finished state tutorial

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -4,17 +4,36 @@ import ReactDOM from 'react-dom';;
 class App extends React.Component{
     constructor(props){
         super(props); //references to the parent constructor function **IMPORTANT
-        this.state = { lat: null };
-    }
+        this.state = { 
+            lat: null,
+            errorMessage: ``
+        }; // the only time we do direct assignment to ths.state
 
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({ //setState
+                    lat: position.coords.latitude
+                });
+            }, // success callback
+            (err) => {
+                this.setState({ errorMessage: err.message})
+            } // failure callback
+        );
+    
+    }
     // React says we have to define render!!!!
     render() {
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position), // success callback
-            (err) => console.log(err) // failure callback
+        return (    
+            !this.state.lat
+            ?
+            <div>
+                Error: {this.state.errorMessage}
+            </div> 
+            : <div>
+                Latitude: {this.state.lat}
+            </div>
+            
         );
-
-        return <div> Latitude: </div>;
     }
 }
 
