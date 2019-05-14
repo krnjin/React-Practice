f5cef9ee97fc5637da875a7ca5aeb45d565f2a3f
krnjin <krnjin94@gmail.com>
2019-05-14 00:43:12 -0700

    53

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';;
 
-const App = () => {
-    return <div>Hi there!</div>;
+class App extends React.Component{
+    constructor(props){
+        super(props); //references to the parent constructor function **IMPORTANT
+        this.state = { lat: null };
+    }
+
+    // React says we have to define render!!!!
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position), // success callback
+            (err) => console.log(err) // failure callback
+        );
+
+        return <div> Latitude: </div>;
+    }
 }
 
 ReactDOM.render(
